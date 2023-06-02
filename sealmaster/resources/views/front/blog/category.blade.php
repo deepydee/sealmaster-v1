4b6dbac6195ad65e01a5747441319e6a22f7154e
Vitaliy Semenov <synthex@bk.ru>
2023-06-02 19:18:08 +0600

    refactor: refactor blog categories and tags appearance

--- sealmaster/resources/views/front/blog/category.blade.php
+++ sealmaster/resources/views/front/blog/category.blade.php
@@ -4,38 +4,52 @@
 
 @section('content')
 
-<section class="container">
+<section class="container-fluid">
     <header>
-      <h1 class="display-5 fw-bold mb-5">{{ $category->title }}</h1>
+        <div class="py-5 bg-light">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6">
+                        <span>Категория</span>
+                        <h3 class="mt-3 fw-bold text-capitalize">{{ $category->title }}</h3>
+                        <p>Category description here.. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam
+                            error eius quo, officiis non maxime quos reiciendis perferendis doloremque maiores!</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </header>
 
-    <nav aria-label="breadcrumb">
-        <ol class="breadcrumb">
-            <li class="breadcrumb-item"><a href="{{ route('home') }}">Главная</a></li>
-            <li class="breadcrumb-item"><a href="{{ route('blog.index') }}">Блог</a></li>
-            <li class="breadcrumb-item active">{{ $category->title }}</li>
-        </ol>
-    </nav>
+    <div class="container">
+        <nav class="mt-4">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="{{ route('home') }}">Главная</a></li>
+                <li class="breadcrumb-item"><a href="{{ route('blog.index') }}">Блог</a></li>
+                <li class="breadcrumb-item active">{{ $category->title }}</li>
+            </ol>
+        </nav>
 
-    <p class="mb-5"></p>
-    <div class="row mb-5">
-      <div class="col-md-9 mb-5">
-        <div class="row g-4">
+        <p class="mb-5"></p>
+        <div class="row mb-5">
+            <div class="col-md-9 mb-5">
+                <div class="row g-4">
 
-            @foreach ($posts as $post)
-            <div class="col-sm-6 col-lg-4">
-                @include('front.post-card', ['post' => $post])
+                    @foreach ($posts as $post)
+                    <div class="col-sm-6 col-lg-4">
+                        @include('front.post-card', ['post' => $post])
+                    </div>
+                    @endforeach
+
+                </div>
             </div>
-            @endforeach
 
+            <aside class="col-md-3 text-start mb-4 categories-list">
+                @include('front.sidebar')
+            </aside>
+
+            {{ $posts->links('vendor.pagination.bootstrap-5') }}
         </div>
-      </div>
-
-      <aside class="col-md-3 text-start mb-4 categories-list">
-        @include('front.sidebar')
-      </aside>
-
-      {{ $posts->links('vendor.pagination.bootstrap-5') }}
     </div>
-  </section>
+
+</section>
 @endsection
--- sealmaster/resources/views/front/blog/tag.blade.php
+++ sealmaster/resources/views/front/blog/tag.blade.php
@@ -4,38 +4,49 @@
 
 @section('content')
 
-<section class="container">
+<section class="container-fluid">
     <header>
-      <h1 class="display-5 fw-bold mb-5">{{ $tag->title }}</h1>
+        <div class="py-5 bg-light">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6">
+                        <span>Тег</span>
+                        <h3 class="mt-3 fw-bold text-capitalize">{{ $tag->title }}</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
     </header>
 
-    <nav aria-label="breadcrumb">
-        <ol class="breadcrumb">
-            <li class="breadcrumb-item"><a href="{{ route('home') }}">Главная</a></li>
-            <li class="breadcrumb-item"><a href="{{ route('blog.index') }}">Блог</a></li>
-            <li class="breadcrumb-item active">{{ $tag->title }}</li>
-        </ol>
-    </nav>
+    <div class="container">
+        <nav class="mt-4" aria-label="breadcrumb">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="{{ route('home') }}">Главная</a></li>
+                <li class="breadcrumb-item"><a href="{{ route('blog.index') }}">Блог</a></li>
+                <li class="breadcrumb-item active">{{ $tag->title }}</li>
+            </ol>
+        </nav>
 
-    <p class="mb-5"></p>
-    <div class="row mb-5">
-      <div class="col-md-9 mb-5">
-        <div class="row g-4">
+        <p class="mb-5"></p>
+        <div class="row mb-5">
+            <div class="col-md-9 mb-5">
+                <div class="row g-4">
 
-            @foreach ($posts as $post)
-            <div class="col-sm-6 col-lg-4">
-                @include('front.post-card', ['post' => $post])
+                    @foreach ($posts as $post)
+                    <div class="col-sm-6 col-lg-4">
+                        @include('front.post-card', ['post' => $post])
+                    </div>
+                    @endforeach
+
+                </div>
             </div>
-            @endforeach
 
+            <aside class="col-md-3 text-start mb-4 categories-list">
+                @include('front.sidebar')
+            </aside>
+
+            {{ $posts->links('vendor.pagination.bootstrap-5') }}
         </div>
-      </div>
-
-      <aside class="col-md-3 text-start mb-4 categories-list">
-        @include('front.sidebar')
-      </aside>
-
-      {{ $posts->links('vendor.pagination.bootstrap-5') }}
     </div>
-  </section>
+</section>
 @endsection
