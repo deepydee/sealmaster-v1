101f7c47ee3ceb042dbe067b73b3acf0e94445a8
Vitaliy Semenov <synthex@bk.ru>
2023-06-03 15:00:30 +0600

    refactor: install ide helper

---
+++ sealmaster/_ide_helper_models.php
@@ -0,0 +1,162 @@
+<?php
+
+// @formatter:off
+/**
+ * A helper file for your Eloquent Models
+ * Copy the phpDocs from this file to the correct Model,
+ * And remove them from this file, to prevent double declarations.
+ *
+ * @author Barry vd. Heuvel <barryvdh@gmail.com>
+ */
+
+
+namespace App\Models{
+/**
+ * App\Models\BlogCategory
+ *
+ * @property int $id
+ * @property string $title
+ * @property string|null $slug
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\BlogPost> $posts
+ * @property-read int|null $posts_count
+ * @method static \Database\Factories\BlogCategoryFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder|BlogCategory findSimilarSlugs(string $attribute, array $config, string $slug)
+ * @method static \Illuminate\Database\Eloquent\Builder|BlogCategory newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|BlogCategory newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|BlogCategory query()
+ * @method static \Illuminate\Database\Eloquent\Builder|BlogCategory whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|BlogCategory whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|BlogCategory whereSlug($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|BlogCategory whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|BlogCategory whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|BlogCategory withUniqueSlugConstraints(\Illuminate\Database\Eloquent\Model $model, string $attribute, array $config, string $slug)
+ */
+	class BlogCategory extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * App\Models\BlogPost
+ *
+ * @property int $id
+ * @property string $title
+ * @property string|null $slug
+ * @property string|null $description
+ * @property array|null $keywords
+ * @property string $content
+ * @property int $is_published
+ * @property int $user_id
+ * @property int $blog_category_id
+ * @property int $views
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \App\Models\BlogCategory $category
+ * @property-read \Spatie\MediaLibrary\MediaCollections\Models\Collections\MediaCollection<int, \Spatie\MediaLibrary\MediaCollections\Models\Media> $media
+ * @property-read int|null $media_count
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\BlogTag> $tags
+ * @property-read int|null $tags_count
+ * @property-read \App\Models\User $user
+ * @method static \Database\Factories\BlogPostFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder|BlogPost findSimilarSlugs(string $attribute, array $config, string $slug)
+ * @method static \Illuminate\Database\Eloquent\Builder|BlogPost newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|BlogPost newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|BlogPost query()
+ * @method static \Illuminate\Database\Eloquent\Builder|BlogPost search($q)
+ * @method static \Illuminate\Database\Eloquent\Builder|BlogPost whereBlogCategoryId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|BlogPost whereContent($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|BlogPost whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|BlogPost whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|BlogPost whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|BlogPost whereIsPublished($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|BlogPost whereKeywords($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|BlogPost whereSlug($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|BlogPost whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|BlogPost whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|BlogPost whereUserId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|BlogPost whereViews($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|BlogPost withUniqueSlugConstraints(\Illuminate\Database\Eloquent\Model $model, string $attribute, array $config, string $slug)
+ */
+	class BlogPost extends \Eloquent implements \Spatie\MediaLibrary\HasMedia {}
+}
+
+namespace App\Models{
+/**
+ * App\Models\BlogTag
+ *
+ * @property int $id
+ * @property string $title
+ * @property string|null $slug
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\BlogPost> $posts
+ * @property-read int|null $posts_count
+ * @method static \Database\Factories\BlogTagFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder|BlogTag findSimilarSlugs(string $attribute, array $config, string $slug)
+ * @method static \Illuminate\Database\Eloquent\Builder|BlogTag newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|BlogTag newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|BlogTag query()
+ * @method static \Illuminate\Database\Eloquent\Builder|BlogTag whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|BlogTag whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|BlogTag whereSlug($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|BlogTag whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|BlogTag whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|BlogTag withUniqueSlugConstraints(\Illuminate\Database\Eloquent\Model $model, string $attribute, array $config, string $slug)
+ */
+	class BlogTag extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * App\Models\TemporaryUpload
+ *
+ * @property int $id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Spatie\MediaLibrary\MediaCollections\Models\Collections\MediaCollection<int, \Spatie\MediaLibrary\MediaCollections\Models\Media> $media
+ * @property-read int|null $media_count
+ * @method static \Illuminate\Database\Eloquent\Builder|TemporaryUpload newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|TemporaryUpload newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|TemporaryUpload query()
+ * @method static \Illuminate\Database\Eloquent\Builder|TemporaryUpload whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|TemporaryUpload whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|TemporaryUpload whereUpdatedAt($value)
+ */
+	class TemporaryUpload extends \Eloquent implements \Spatie\MediaLibrary\HasMedia {}
+}
+
+namespace App\Models{
+/**
+ * App\Models\User
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $email
+ * @property \Illuminate\Support\Carbon|null $email_verified_at
+ * @property mixed $password
+ * @property string|null $remember_token
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Spatie\MediaLibrary\MediaCollections\Models\Collections\MediaCollection<int, \Spatie\MediaLibrary\MediaCollections\Models\Media> $media
+ * @property-read int|null $media_count
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection<int, \Illuminate\Notifications\DatabaseNotification> $notifications
+ * @property-read int|null $notifications_count
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \Laravel\Sanctum\PersonalAccessToken> $tokens
+ * @property-read int|null $tokens_count
+ * @method static \Database\Factories\UserFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder|User newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|User newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|User query()
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereEmailVerifiedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User wherePassword($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereRememberToken($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereUpdatedAt($value)
+ */
+	class User extends \Eloquent implements \Spatie\MediaLibrary\HasMedia {}
+}
+
